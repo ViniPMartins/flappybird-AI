f2b950a53a0a138007af91223f9fc716d2c688a5
ViniPMartins <vinicius.pereira.mts@gmail.com>
2023-07-01 23:32:33 -0300

    Atualização movimentos do passaro e Rede Neural

--- AI/_rede_neural.py
+++ AI/_rede_neural.py
@@ -3,7 +3,8 @@ from tensorflow.keras.layers import Dense
 
 def create_model(input_shape):
     model = Sequential([
-        Dense(3, input_shape=(input_shape,), activation='relu'),
+        Dense(8, input_shape=(input_shape,), activation='relu'),
+        Dense(4, activation='relu'),
         Dense(1, activation='sigmoid')
     ])
 
