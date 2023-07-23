cdb80a28f0da4f857a75142ca8d84b5c3a0656d9
ViniPMartins <vinicius.pereira.mts@gmail.com>
2023-07-23 16:42:05 -0300

    Construcao Perceptron

--- AI/_rede_neural.py
+++ AI/_rede_neural.py
@@ -1,13 +1,47 @@
-from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Dense, InputLayer
-from keras.initializers import RandomNormal
+import numpy as np
+import math
+import os
+
+class Perceptron(object):
+
+    def __init__(self, input_shape, output=1):
+        self.weights = np.random.randn(input_shape,output)
+        self.bias = np.random.randn(output,)
+        self.parameters = np.array([self.weights, self.bias], dtype=object)
+           
+    def predict(self, inputs):
+        summation = np.dot(inputs, self.parameters[0]) + self.parameters[1]           
+        activation = [math.tanh(x) for x in summation]
+        return activation
+    
+    def get_weights(self):
+        return self.parameters
+    
+    def set_weights(self, new_parameters):
+        self.parameters = new_parameters
+
+    def save_params(self):
+        folder_name = "checkpoint"
+        file_name = "saved_params.npy"
+
+        if not os.path.isdir(folder_name):
+            os.mkdir(folder_name)
+
+        filepath = os.path.join(folder_name, file_name)
+
+        np.save(filepath, self.parameters, allow_pickle=True)
+        return print("Paramters saved in: ", filepath)
+    
+    def load_params(self):
+        folder_name = "checkpoint"
+        file_name = "saved_params.npy"
+        filepath = os.path.join(folder_name, file_name)
+        
+        self.parameters = np.load(filepath, allow_pickle=True)
+        return print("Paramters load")
 
 def create_model(input_shape):
-    model = Sequential([
-        InputLayer(input_shape=(input_shape,)),
-        Dense(1, activation='tanh', bias_initializer=RandomNormal(0,1))
-    ])
-
+    model = Perceptron(input_shape)
     return model
 
 def create_weights(input_shape):
