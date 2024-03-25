07938d17dc084e48cb3f7f441fb05ecdba4d5972
ViniPMartins <vinicius.pereira.mts@gmail.com>
2024-03-25 20:16:28 -0300

    Add tests to FlappyBird.py

--- test_FlappyBird.py
+++ test_FlappyBird.py
@@ -11,8 +11,6 @@ class MockModel:
     
     def get_weights(self):
         return "get_weights"
-    
-mockPopulation = [[999, 999, 999, 999]]
 
 def test_init():
     newGame = Game(False)
@@ -24,13 +22,19 @@ def test_init():
     assert newGame.input_shape == 3
     assert newGame.use_trained_model == False
 
-# def test_init_generations(mocker):
-#     newGame = Game(False)
+def test_init_generations(mocker):
+    mocker.patch('FlappyBird.Game.new_training_routine', return_value="new_training_routine")
+    mocker.patch('FlappyBird.Game.using_trained_model', return_value="using_trained_model")
+    
+    newGame = Game(False)
+    assert newGame.use_trained_model == False
 
-#     mocker.patch('FlappyBird.Game.new_training_routine', return_value="new_training_routine")
-#     assert newGame.init_generations() == 'new_training_routine'
+    newGame = Game(True)
+    assert newGame.use_trained_model == True
+
 
 def test_using_trained_model(mocker):
+    mockPopulation = [[999, 999, 999, 999]]
     mocker.patch('FlappyBird.create_new_population', return_value=mockPopulation)
     mocker.patch('FlappyBird.create_model', return_value=MockModel())
     mocker.patch('FlappyBird.Game.main', return_value=True)
@@ -41,3 +45,17 @@ def test_using_trained_model(mocker):
     assert newGame.population == [[999,999,'get_weights',999]]
     assert isinstance(newGame.model, MockModel)
     assert newGame.learning_threshold == 100000
+
+def test_new_training_routine(mocker):
+    mockPopulation = [[999, 999, 999, 999]]
+    mocker.patch('FlappyBird.create_model', return_value=MockModel())
+    mocker.patch('FlappyBird.create_new_population', return_value=mockPopulation)
+    mocker.patch('FlappyBird.new_generation', return_value=mockPopulation)
+    mocker.patch('FlappyBird.Game.main', return_value=True)
+    mocker.patch('FlappyBird.Game.print_results', return_value=True)
+
+    newGame2 = Game()
+    newGame2.new_training_routine()
+    assert isinstance(newGame2.model, MockModel)
+    assert newGame2.population == [[999,999,999,999]]
+    assert newGame2.geracao == newGame2.num_geracoes
