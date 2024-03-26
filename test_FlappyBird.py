ada3d1e42aad9b0bb76178edbc387bee614fceb6
ViniPMartins <vinicius.pereira.mts@gmail.com>
2024-03-25 21:38:42 -0300

    Refactoring tests

--- test_FlappyBird.py
+++ test_FlappyBird.py
@@ -12,50 +12,76 @@ class MockModel:
     def get_weights(self):
         return "get_weights"
 
-def test_init():
-    newGame = Game(False)
+def test_init_using_trained_neural_network():
+    use_neural_net_model = True
+    use_trained_model = True
+    newGame = Game(use_neural_net_model, use_trained_model)
+
+    assert newGame.num_population == 1
+    assert newGame.num_geracoes == 1
+    assert newGame.geracao == 0
+    assert newGame.learning_threshold == 1000
+    assert newGame.input_shape == 3
+    assert newGame.use_neural_net_model == True
+    assert newGame.use_trained_model == True
+
+    use_neural_net_model = False
+    use_trained_model = True
+    newGame = Game(use_neural_net_model, use_trained_model)
+    assert newGame.use_neural_net_model == False
+    assert newGame.use_trained_model == None
+    assert newGame.num_population == 1
+    assert newGame.num_geracoes == 1
+
+
+def test_init_new_training_neural_network():
+    use_neural_net_model = True
+    use_trained_model = False
+    newGame = Game(use_neural_net_model, use_trained_model)
 
     assert newGame.num_population == 100
     assert newGame.num_geracoes == 300
     assert newGame.geracao == 0
     assert newGame.learning_threshold == 1000
     assert newGame.input_shape == 3
+    assert newGame.use_neural_net_model == True
     assert newGame.use_trained_model == False
 
-def test_init_generations(mocker):
-    mocker.patch('FlappyBird.Game.new_training_routine', return_value="new_training_routine")
-    mocker.patch('FlappyBird.Game.using_trained_model', return_value="using_trained_model")
-    
-    newGame = Game(False)
-    assert newGame.use_trained_model == False
+def test_init_with_manual_calculus():
+    use_neural_net_model = False
+    newGame = Game(use_neural_net_model)
 
-    newGame = Game(True)
-    assert newGame.use_trained_model == True
-
-
-def test_using_trained_model(mocker):
-    mockPopulation = [[999, 999, 999, 999]]
-    mocker.patch('FlappyBird.create_new_population', return_value=mockPopulation)
-    mocker.patch('FlappyBird.create_model', return_value=MockModel())
-    mocker.patch('FlappyBird.Game.main', return_value=True)
-    
-    newGame = Game(True)
-    newGame.using_trained_model()
     assert newGame.num_population == 1
-    assert newGame.population == [[999,999,'get_weights',999]]
-    assert isinstance(newGame.model, MockModel)
-    assert newGame.learning_threshold == 100000
+    assert newGame.num_geracoes == 1
+    assert newGame.geracao == 0
+    assert newGame.learning_threshold == 1000
+    assert newGame.input_shape == 3
+    assert newGame.use_neural_net_model == False
+    assert newGame.use_trained_model == None
 
-def test_new_training_routine(mocker):
+def test_init_game(mocker):
     mockPopulation = [[999, 999, 999, 999]]
-    mocker.patch('FlappyBird.create_model', return_value=MockModel())
-    mocker.patch('FlappyBird.create_new_population', return_value=mockPopulation)
-    mocker.patch('FlappyBird.new_generation', return_value=mockPopulation)
+    mocker.patch('FlappyBird.Game.setup_population', return_value="setup_population")
     mocker.patch('FlappyBird.Game.main', return_value=True)
     mocker.patch('FlappyBird.Game.print_results', return_value=True)
+    mocker.patch('FlappyBird.new_generation', return_value=mockPopulation)
 
-    newGame2 = Game()
-    newGame2.new_training_routine()
-    assert isinstance(newGame2.model, MockModel)
-    assert newGame2.population == [[999,999,999,999]]
-    assert newGame2.geracao == newGame2.num_geracoes
+    newGame = Game(use_trained_model=False)
+    assert newGame.use_trained_model == False
+
+    newGame = Game(use_trained_model=True)
+    assert newGame.use_trained_model == True
+
+def test_increase_fitness():
+    mockPopulation = [[999, 0, 999, 999]]
+    newGame = Game()
+    newGame.population = mockPopulation
+    newGame.increase_fitness(0)
+    assert newGame.population == [[999,0.1,999,999]]
+
+def test_increse_fitness_passing_cano():
+    mockPopulation = [[999, 0, 999, 999]]
+    newGame = Game()
+    newGame.population = mockPopulation
+    newGame.increse_fitness_passing_cano(0)
+    assert newGame.population == [[999,20,999,999]]
